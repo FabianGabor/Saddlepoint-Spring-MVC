67297ce234a0e13673685ce4e31b1b94ad5e6efb
Fábián Gábor <gabor_fabian1@epam.com>
2022-03-29 20:20:30 +0200

    init

---
+++ src/main/java/hu/nye/fabiangabor/saddlepoint/SaddlePointApp.java
@@ -0,0 +1,13 @@
+package hu.nye.fabiangabor.saddlepoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaddlePointApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaddlePointApp.class, args);
+	}
+
+}
